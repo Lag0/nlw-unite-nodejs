b1f0a3f948bd91164ebbaab44e02f9f0eefba00e
Lag0 <brunolago@incorppar.com>
2024-04-08 15:28:00 -0300

    feat: event info by slug

---
+++ src/routes/get-event-by-slug.ts
@@ -0,0 +1,74 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { BadRequest } from "./_errors/bad-request";
+
+export async function getEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/events/slug/:slug",
+    {
+      schema: {
+        summary: "Get details of an event",
+        tags: ["Events"],
+        params: z.object({
+          slug: z.string(),
+        }),
+        response: {
+          200: z.object({
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              slug: z.string(),
+              details: z.string().nullable(),
+              maximumAttendees: z.number().nullable(),
+              currentAttendees: z.number(),
+              price: z.number().nullable(),
+              createdAt: z.date(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { slug } = request.params;
+
+      const event = await prisma.event.findUnique({
+        where: {
+          slug,
+        },
+        select: {
+          id: true,
+          title: true,
+          slug: true,
+          details: true,
+          maximumAttendees: true,
+          price: true,
+          createdAt: true,
+          _count: {
+            select: {
+              attendees: true,
+            },
+          },
+        },
+      });
+
+      if (event === null) {
+        throw new BadRequest("Event not found"); // 404
+      }
+
+      return reply.send({
+        event: {
+          id: event.id,
+          title: event.title,
+          slug: event.slug,
+          details: event.details,
+          maximumAttendees: event.maximumAttendees,
+          currentAttendees: event._count.attendees,
+          price: event.price,
+          createdAt: event.createdAt,
+        },
+      });
+    }
+  );
+}
